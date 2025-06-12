33b3438384a41696156ab47aee11adc8d8f2bfee
abandon <3240529156@qq.com>
2025-06-12 18:27:08 +0800

    项目初始化

---
+++ src/main/java/com/aitourismplanningmaster/AiTourismPlanningMasterApplication.java
@@ -0,0 +1,13 @@
+package com.aitourismplanningmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiTourismPlanningMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiTourismPlanningMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aitourismplanningmaster/AiTourismPlanningMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aitourismplanningmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiTourismPlanningMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
